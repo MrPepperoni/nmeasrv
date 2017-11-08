0257bf837bf8ad57d36ecea495ad0f861d9ba031
aron ujvary <aronujvary@gmail.com>
2017-11-08 18:07:27 +0100

    input from file

--- nmeasrv.py
+++ nmeasrv.py
@@ -7,18 +7,32 @@ import operator
 import threading
 import time
 import datetime
+import argparse
 from functools import reduce
 from geopy.geocoders import Nominatim
 
 
-startutc = datetime.datetime(2018,3,4,9,43,27,320000)
-delta = datetime.timedelta()
-location = 'Budapest, Hungary'
+startutc = datetime.datetime.now()
+zeroutc = datetime.datetime.now()
 
 geolocator = Nominatim()
-geoloc = geolocator.geocode(location)
-if not geoloc:
-    raise ValueError('Requested location not found')
+geoloc = None
+
+def switch_to(timest, locstr):
+    global zeroutc
+    global startutc
+    global geoloc
+    geoloc = None
+    logger = logging.getLogger('Switch')
+    if True:
+        startutc = datetime.datetime.strptime(timest.strip(),"%Y-%m-%d %H:%M:%S.%f")
+        zeroutc = datetime.datetime.now()
+        geoloc = geolocator.geocode(locstr.strip())
+        logger.info('Switched to %s %s', timest, locstr)
+        return True
+    if False:
+        logger.warning('Cannot parse %s %s', timest, locstr)
+        return False
 
 def degrees(loc):
     return int(("%f" % abs(loc)).split('.')[0])
@@ -40,6 +54,7 @@ def lon(gc):
     return "%03d%07.4f,%s" % (degrees(loc), minutes(loc), (loc < 0 and 'W' or 'E'))
 
 def curtime():
+    delta = datetime.datetime.now() - zeroutc
     return startutc + delta
 
 def timestr():
@@ -117,12 +132,11 @@ class SenderThread(threading.Thread):
         self._stop = True
 
     def run(self):
-        zero = datetime.datetime.now()
+        global geoloc
         while self._stop == False:
-            global delta
-            delta = datetime.datetime.now() - zero
-            for s in sentences:
-                self.client.enqueueData(s.tostring())
+            if geoloc:
+                for s in sentences:
+                    self.client.enqueueData(s.tostring())
             time.sleep(1)
 
 class ClientHandler(asyncore.dispatcher):
@@ -155,20 +169,52 @@ class ClientHandler(asyncore.dispatcher):
     def handle_read(self):
         data = self.recv(1024)
         self.logger.debug('handle_read() -> (%d) "%s"', len(data), data.rstrip())
-        # !!!!! EXAMPLE - ECHO
-        # self.data_to_write.insert(0, data)
     
     def handle_close(self):
         self.logger.debug('handle_close()')
         self.t.stop()
         self.close()
 
+class TestCaseHandler(threading.Thread):
+    _stop = False
+
+    def __init__(self, infile):
+        super(TestCaseHandler,self).__init__()
+        self.infile = infile
+
+    def stop(self):
+        self._stop = True
+
+    def run(self):
+        for l in self.infile.readlines():
+            parts = l.split('@')
+            if not switch_to(parts[0],parts[1]):
+                continue
+            for i in range(5 * 60):
+                if self._stop:
+                    return
+                time.sleep(1)  # wait 5 minutes
+
+
+
 def main():
     logging.basicConfig(level=logging.DEBUG, format='%(name)s:[%(levelname)s]: %(message)s')
-    HOST = '0.0.0.0'
-    PORT = 9999
-    s = Server((HOST,PORT))
+
+    parser = argparse.ArgumentParser(description='Create NMEA emulator tcp server')
+    parser.add_argument('-p', '--port', dest='port', type=int,
+                        default=9999,
+                        help='port to listen on (default: 9999)')
+    parser.add_argument('infile', type=argparse.FileType('r'))
+
+    args = parser.parse_args()
+    print(args.port)
+
+    host = '0.0.0.0'
+    s = Server((host,args.port))
+    t = TestCaseHandler(args.infile)
+    t.run()
     asyncore.loop(1)
+    t.stop()
 
 
 if __name__ == '__main__':
